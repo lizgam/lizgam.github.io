249174c0ccbee5765047702be2638cc516e882ec
Liza Hamaliy <liza.gamaliy@gmail.com>
2017-12-04 23:12:13 +0300

    add task with babel

--- newsapi/app/constants.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = '4c9833664e2b46b6a433407c24c2f128';
--- newsapi/app/index.js
+++
@@ -1,8 +0,0 @@
-import { Configurator } from './configurator.js';
-
-window.addEventListener("load", newsConfiguration);
-
-function newsConfiguration() {
-  let configurator = new Configurator();
-  configurator.initPage();
-}
